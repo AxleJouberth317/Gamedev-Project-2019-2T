3c2a4cb06363c2b44389e3d65ea568d6ce18599d
AxleJouberth317 <39098532+AxleJouberth317@users.noreply.github.com>
2019-12-16 04:56:00 -0500

    First Level Basics
    Basic Mechanics, and enviroment spawning

---
+++ Menu/Assets/Scripts/Controls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controls : MonoBehaviour
+{
+
+
+    public float tapForce = 10;
+    public float smooth = 5;
+    public Vector3 startPosition;
+    Rigidbody2D rigidbody;
+    Quaternion down;
+    Quaternion up;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        down = Quaternion.Euler(0, 0, 0);
+        up = Quaternion.Euler(0, 0, 10);
+        //rigidbody.simulated = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = up;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, down, smooth * Time.deltaTime);
+
+    }
+
+    void OnCoinCollected(Collider2D col)
+    {
+        if (col.gameObject.tag == "Coin")
+        {
+
+        }
+    }
+
+    void OnDanger(Collider2D col)
+    {
+        if (col.gameObject.tag == "Danger")
+        {
+            rigidbody.simulated = false;
+        }
+    }
+
+}
---
+++ Menu/Assets/Scripts/Management.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Management : MonoBehaviour
+{
+    public static Management Instance;
+
+    /*
+    public delegate void Delegate();
+    public static event Delegate OnGameStart;
+    public static event Delegate OnGameOver;
+    */
+
+    public GameObject menuPage;
+    public GameObject startPage;
+    public GameObject gameOverPage;
+
+    //public Text scoreText;
+
+    enum PageState { 
+        None,
+        Menu,
+        Start,
+        GameOver,
+    }
+
+
+    bool gameOver = false;
+    public bool GameOver { get { return gameOver; } }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                menuPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                break;
+            case PageState.Menu:
+                startPage.SetActive(false);
+                menuPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                menuPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                menuPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                break;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
